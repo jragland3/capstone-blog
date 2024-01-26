2e1e2de8d8beae97ae3e938ec6342467542e93d5
Josh Ragland <59399189+jragland3@users.noreply.github.com>
2024-01-26 14:20:01 -0600

    add gitignore

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # capstone-blog
 Udemy course capstone project for developing a simple blog post website
+
+Project Description
+
+The goal of this project is to create a Blog web application using Node.js, Express.js, and EJS. The application will allow users to create and view blog posts. Posts will not persist between sessions as no database will be used in this version of the application. Styling will be an important aspect of this project to ensure a good user experience.
+
+
+Install Dependencies
+`npm i`
+
+Run application
+`node index.js`
---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+let authorList = [
+  "Josh Ragland",
+  "Josh Ragland",
+  "Dudebrah McGee"
+]
+let postList = [
+  "This is my first post!",
+  "Hello again!",
+  "Just hanging out"
+]
+let allPosts = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(createPostList);
+
+class Post {
+  constructor(author, postText) {
+    this.author = author;
+    this.postText = postText;
+  };
+};
+
+function createPostList(req, res, next) {
+  allPosts = [];
+  for (let i=0; i < authorList.length; i++) {
+    let post = new Post(authorList[i], postList[i]);
+    allPosts.push(post);
+  };
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { allPosts });
+});
+
+app.get("/create_post", (req, res) => {
+  res.render("create_post.ejs", { allPosts });
+});
+
+app.post("/submit", (req, res) => {
+  authorList.push(req.body.name);
+  postList.push(req.body['post-text']);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
